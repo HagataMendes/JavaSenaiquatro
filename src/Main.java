370cc0f2c4a2c4491afbf37151453e237fb55558
HagataMendes <hagatamendes2017@gmail.com>
2025-03-22 10:17:20 -0300

    feat: JavaSenaiaulaquatro

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int num = 229;
+        Long num04 = 10L;
+        byte numNovo = -128;
+        byte numNovo02 = 127;
+        System.out.println(num+num04);
+        System.out.println(num-num04);
+        System.out.println(num/num04);
+        System.out.println(num%num04);
+
+        System.out.println(numNovo*numNovo02);
+
+        System.out.println("-------------------------------------------");
+        String nome = "Hagata";
+        System.out.println();
+
+        char status = 'H';
+        char status1 = 'A';
+        char status2 = 'G';
+        char status3 = 'A';
+        char status4 = 'T';
+        char status5 = 'A';
+        System.out.println(status+status1+status2+status3+status4+status5);
+        System.out.println(""+status+status1+status2+status3+status4+status5);
+
+
+
+
+
+
+
+
+
+
+
+//        System.out.println("-------------------------------------------");
+//
+//        Float num02 = 217.5f;
+//        Double num03 = 10.5;
+//        System.out.println(num+num02);
+//        System.out.println(num-num02);
+//        System.out.println(num/num02);
+//        System.out.println(num%num02);
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+        }
+    }
